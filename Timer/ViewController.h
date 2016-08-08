c602e0bf1e914ba7e37ebf6f1ef52d4d77c61228
luyunbo <luyunbo47@gmail.com>
2016-08-08 10:19:15 +0800

    Initial Commit

---
+++ Timer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Timer
+//
+//  Created by luyunbo on 8/8/16.
+//  Copyright © 2016 luyunbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Timer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Timer
+//
+//  Created by luyunbo on 8/8/16.
+//  Copyright © 2016 luyunbo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
