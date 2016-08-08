2d52ab0b4d03cce4c67ba95de6d43c045af051da
luyunbo <luyunbo47@gmail.com>
2016-08-08 22:07:46 +0800

    # 开启和关闭定时器

--- Timer/ViewController.m
+++ Timer/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property(weak,nonatomic) NSTimer *timer;
+
 @end
 
 @implementation ViewController
@@ -17,6 +20,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    NSLog(@"timer start");
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(timerFunc) userInfo:nil repeats:YES];
+}
+
+
+-(void)viewDidDisappear:(BOOL)animated {
+    [_timer invalidate];
+    _timer = nil;
+    NSLog(@"timer stop");
+}
+
+- (void)timerFunc
+{
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    
+    [formatter setDateFormat:@"MM/dd/YY HH:mm:ss"];
+    
+    NSString *timestamp = [formatter stringFromDate:[NSDate date]];
+    
+    [_timeLabel setText:timestamp];//时间在变化的语句
+    
+//    NSLog(@"%@",timestamp);
+    
 }
 
 - (void)didReceiveMemoryWarning {
